b773b872c5f3ae1bf114c9d67d7b5aee954f1566
agonbar117 <agb132smr@gmail.com>
2020-04-26 21:49:16 +0200

    Devuelve un array de objetos con todos los nombres y URL de las pestañas

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 "use strict"
-
+let tabActual={};
+let resultado=[];
 
 chrome.browserAction.onClicked.addListener(btnClicked)
 
 function btnClicked() {
     let querying = chrome.tabs.query({}, function logTabs(tabs) {
         for (let tab of tabs) {
-        console.log(tab.title+ ": "+tab.url);
+            tabActual={
+                Nombre: tab.title,
+                URL: tab.url
+            }
+
+            resultado.push(tabActual);
         }
     });
 }
