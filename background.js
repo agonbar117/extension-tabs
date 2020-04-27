485d77c1bd9eb244750a44474ab70423ec08564e
agonbar117 <agb132smr@gmail.com>
2020-04-28 01:36:32 +0200

    Descarga en un fichero txt los <title> y las URL

--- background.js
+++ background.js
@@ -1,18 +1,21 @@
 "use strict"
 let tabActual={};
 let resultado=[];
-chrome.browserAction.onClicked.addListener(btnClicked)
+chrome.browserAction.onClicked.addListener(btnClicked);
 
 function btnClicked() {
     let querying = chrome.tabs.query({}, function logTabs(tabs) {
         for (let tab of tabs) {            
             tabActual={
             Nombre: tab.title,
-            URL: tab.url
+            URL: tab.url,
+            toString: function(){return "Nombre: "+this.Nombre+" - URL: "+this.URL+"\n";}
         }
+
         resultado.push(tabActual);
         }
-        let blob= new Blob([resultado], {type: "text/html"});
+       
+        let blob= new Blob([resultado], {type: "text/plain; charset=UTF-8"});
         let url= URL.createObjectURL(blob);
         chrome.downloads.download({
             url:url,
